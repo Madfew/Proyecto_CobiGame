596721c26427df65070c7cd784e54b96b6fe2e29
Maria Fernanda Flores <mafer_271099@live.com>
2018-06-25 17:26:16 -0500

    Detalles del nivel.

--- COBI/ProyectoCobi_DreamTeam/Assets/Scripts/CreacionProcedural.cs
+++ COBI/ProyectoCobi_DreamTeam/Assets/Scripts/CreacionProcedural.cs
@@ -5,21 +5,52 @@ using UnityEngine;
 public class CreacionProcedural : MonoBehaviour {
 
     public GameObject[] tiles;
-    public int[] orden;
+    public int[] orden1;
+    public int[] orden2;
+    public int[] orden3;
+    public int[] orden4;
 
     private Vector3 puntoInicio;
 
 	// Use this for initialization
 	void Start () {
-		for(int i=0; i <orden.Length; i++)
-        {
-            GameObject tile = Instantiate(tiles[orden[i]], puntoInicio, Quaternion.identity);
-            puntoInicio = tile.transform.Find("Helper").position;
-        }
+        CreacionTileNivel1();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void CreacionTileNivel1()
+    {
+        for (int i = 0; i < orden1.Length; i++)
+        {
+            GameObject tile = Instantiate(tiles[orden1[i]], puntoInicio, Quaternion.identity);
+            puntoInicio = tile.transform.Find("Helper").position;
+        }
+
+        for (int i = 0; i < orden2.Length; i++)
+        {
+            GameObject tile = Instantiate(tiles[orden2[i]], puntoInicio, Quaternion.identity);
+            puntoInicio = tile.transform.Find("Helper").position;
+        }
+
+    }
+
+    public void CreacionTileNivel2()
+    {
+        for (int i = 0; i < orden3.Length; i++)
+        {
+            GameObject tile = Instantiate(tiles[orden3[i]], puntoInicio, Quaternion.identity);
+            puntoInicio = tile.transform.Find("Helper").position;
+        }
+
+        for (int i = 0; i < orden4.Length; i++)
+        {
+            GameObject tile = Instantiate(tiles[orden4[i]], puntoInicio, Quaternion.identity);
+            puntoInicio = tile.transform.Find("Helper").position;
+        }
+    }
+
 }
