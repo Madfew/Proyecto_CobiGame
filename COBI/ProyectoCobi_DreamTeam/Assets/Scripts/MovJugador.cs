4c5df096badeec0cf17b5e9284f784d1fbd23654
Maria Fernanda Flores <mafer_271099@live.com>
2018-06-25 15:36:53 -0500

    Cambios en MenuPrincipal.

--- COBI/ProyectoCobi_DreamTeam/Assets/Scripts/MovJugador.cs
+++ COBI/ProyectoCobi_DreamTeam/Assets/Scripts/MovJugador.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets;
 
 public class MovJugador : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    float dirX;
+    Vector3 direccion, salto;
+    Rigidbody rb;
+
+    [Header("MovimientoPersonaje")]
+    [Range(1f, 20f)]
+    public float velocidadMov = 5f;
+
+    [Header("Salto")]
+    [Range(100f, 1500f)]
+    public float saltoFuerza = 500f;
+    public int saltar;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		
 	}
 }
