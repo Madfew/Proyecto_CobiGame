712d7eb31fbd1dff00fda9befc72a4c445a84200
Fernando Chumpitaz <fernandochumpitaz02@gmail.com>
2018-06-29 19:03:21 -0500

    Se añadió el menú de pausa y el script que controla a este

---
+++ COBI/ProyectoCobi_DreamTeam/Assets/Vista/Vista scripts/Menupausa.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menupausa : MonoBehaviour {
+
+     
+
+	public void juegopausa()
+    {
+     
+            Time.timeScale = 0;
+    }
+    public void juegocontinuar()
+    {
+        
+        
+            Time.timeScale = 1;
+    }
+}
+
+
