688249fac64ea75d79906f02abc821ab0903377d
Fernando Chumpitaz <fernandochumpitaz02@gmail.com>
2018-06-30 10:04:14 -0500

    se anadió el código del ui de los hitpoints

--- COBI/ProyectoCobi_DreamTeam/Assets/Vista/Vista scripts/DañoGeneral.cs
+++ COBI/ProyectoCobi_DreamTeam/Assets/Vista/Vista scripts/DañoGeneral.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DañoGeneral : MonoBehaviour
 {
-
-    int vida = 100;
+    public Image[] hitpoints;
+    int vida = 5;
 
 
     // Use this for initialization
@@ -18,21 +19,34 @@ public class DañoGeneral : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        /*
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            GetHurt();
+        }*/
     }
 
     private void OnCollisionEnter(Collision enemigo)
     {
         if (enemigo.gameObject.tag == "Enemigo")
         {
-            vida = vida - 20;
             Destroy(enemigo.gameObject, 1f);
+            GetHurt();
         }
 
+       
+    }
+    void GetHurt()
+    {
+        if (vida > 0)
+        {
+            vida = vida - 1;
+        }
+        hitpoints[vida].gameObject.SetActive(false);
         if (vida == 0)
         {
-            SceneManager.LoadScene(3);
+            SceneManager.LoadScene("Derrota");
         }
     }
-
+   
 }
