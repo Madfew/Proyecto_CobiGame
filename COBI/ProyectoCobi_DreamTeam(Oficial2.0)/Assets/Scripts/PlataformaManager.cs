e702ab2c234a0f93b0c982ca33f06e16f1ba6627
Maria Fernanda Flores <mafer_271099@live.com>
2018-07-02 17:10:12 -0500

    Ampliado y mejorado el nivel 1.

---
+++ COBI/ProyectoCobi_DreamTeam(Oficial2.0)/Assets/Scripts/PlataformaManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaManager : MonoBehaviour {
+
+    public float velocidad;
+    float timer;
+    bool enMov;
+
+	// Use this for initialization
+	void Start () {
+        enMov = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+
+        if (enMov)
+        {
+            transform.Translate(Vector3.back * velocidad * Time.deltaTime);
+        }
+
+        if(enMov == false)
+        {
+            if (timer >= 5f)
+            {
+                enMov = true;
+            }
+        }
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PosicionFinal")
+        {
+            timer = 0f;
+            enMov = false;
+        }
+    }
+}
