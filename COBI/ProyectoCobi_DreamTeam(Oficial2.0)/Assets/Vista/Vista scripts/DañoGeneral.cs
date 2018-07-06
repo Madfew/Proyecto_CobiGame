a2f0d450b567438f693be277cdcf08adb9e0c857
Fernando Chumpitaz <fernandochumpitaz02@gmail.com>
2018-07-06 06:58:52 -0500

    código del daño general simplificado

--- COBI/ProyectoCobi_DreamTeam(Oficial2.0)/Assets/Vista/Vista scripts/DañoGeneral.cs
+++ COBI/ProyectoCobi_DreamTeam(Oficial2.0)/Assets/Vista/Vista scripts/DañoGeneral.cs
@@ -8,10 +8,33 @@ public class DañoGeneral : MonoBehaviour
 {
     public Image[] hitpoints;
     int vida = 5;
+    public Image dañoImagen;
+    public float flashVelocidad = 5f;
+    public Color flashColor = new Color(1, 0f, 0f, 0.1f);
+
+    AudioSource jugadorAudio;
+    MovJugador jugador;
+    bool muerto;
+    bool daño;
+
+    void Start()
+    {
+        jugadorAudio = GetComponent<AudioSource>();
+        jugador = GetComponent<MovJugador>();
+    }
+
 
     void Update()
     {
-
+        if (daño)
+        {
+            dañoImagen.color = flashColor;
+        }
+        else
+        {
+            dañoImagen.color = Color.Lerp(dañoImagen.color, Color.clear, flashVelocidad * Time.deltaTime);
+        }
+        daño = false;
     }
     
     private void OnTriggerEnter(Collider obj)
@@ -27,17 +50,26 @@ public class DañoGeneral : MonoBehaviour
             recuperarvida();
         }
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemigo")
+        {
+            GetHurt();
+        }
+    }
 
     void GetHurt()
     {
+        daño = true;
         if (vida > 0)
         {
             vida = vida - 1;
         }
+        jugadorAudio.Play();
         hitpoints[vida].gameObject.SetActive(false);
-        if (vida == 0)
+        if (vida == 0 && !muerto)
         {
-            SceneManager.LoadScene("Derrota");
+            Muerto();
         }
     }
 
@@ -49,6 +81,12 @@ public class DañoGeneral : MonoBehaviour
         }
         hitpoints[vida - 1].gameObject.SetActive(true);
 
+    }
+
+    void Muerto(){
+        muerto = true;
+        jugador.enabled = false;
+        SceneManager.LoadScene("Derrota");
     }
 }
 
